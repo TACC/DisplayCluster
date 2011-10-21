484b81161bba39338375325089ad450af9847747
Greg Johnson <johnson.gp@gmail.com>
2011-10-21 14:16:52 -0500

    the GUI now shows the tiled display with the correct aspect ratio regardless of the window size.

--- src/DisplayGroupGraphicsView.cpp
+++ src/DisplayGroupGraphicsView.cpp
@@ -1,5 +1,7 @@
 #include "DisplayGroupGraphicsView.h"
 #include "DisplayGroupGraphicsScene.h"
+#include "main.h"
+#include <algorithm>
 
 DisplayGroupGraphicsView::DisplayGroupGraphicsView()
 {
@@ -17,6 +19,31 @@ DisplayGroupGraphicsView::DisplayGroupGraphicsView()
 
 void DisplayGroupGraphicsView::resizeEvent(QResizeEvent * event)
 {
+    // compute the scene rectangle to show such that the aspect ratio corresponds to the actual aspect ratio of the tiled display
+    float tiledDisplayAspect = (float)g_configuration->getTotalWidth() / (float)g_configuration->getTotalHeight();
+    float windowAspect = (float)width() / (float)height();
+
+    float sceneWidth, sceneHeight;
+
+    if(tiledDisplayAspect >= windowAspect)
+    {
+        sceneWidth = 1.;
+        sceneHeight = tiledDisplayAspect / windowAspect;
+    }
+    else // tiledDisplayAspect < windowAspect
+    {
+        sceneHeight = 1.;
+        sceneWidth = windowAspect / tiledDisplayAspect;
+    }
+
+    // make sure we have a small buffer around the (0,0,1,1) scene rectangle
+    float border = 0.05;
+
+    sceneWidth = std::max(sceneWidth, (float)1.+border);
+    sceneHeight = std::max(sceneHeight, (float)1.+border);
+
+    setSceneRect(-(sceneWidth - 1.)/2., -(sceneHeight - 1.)/2., sceneWidth, sceneHeight);
+
     fitInView(sceneRect());
 
     QGraphicsView::resizeEvent(event);
