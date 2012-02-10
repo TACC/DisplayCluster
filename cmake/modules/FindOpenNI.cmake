62b26822c5c7893fe4c081b9b17a5759fbcc6e52
Greg Johnson <johnson.gp@gmail.com>
2012-02-10 12:18:20 -0600

    marked skeleton support experimental. formatting.

--- cmake/modules/FindOpenNI.cmake
+++ cmake/modules/FindOpenNI.cmake
@@ -1,34 +1,34 @@
 # - Try to find OpenNI
 # Once done, this will define
 #
-#  OPENNI_FOUND - system has OpenNI
-#  OPENNI_INCLUDE_DIRS - the OpenNI include directories
-#  OPENNI_LIBRARIES - link these to use OpenNI
+#  OpenNI_FOUND - system has OpenNI
+#  OpenNI_INCLUDE_DIRS - the OpenNI include directories
+#  OpenNI_LIBRARIES - link these to use OpenNI
 #
 # this file is modeled after http://www.cmake.org/Wiki/CMake:How_To_Find_Libraries
 
 include(LibFindMacros)
 
 # Use pkg-config to get hints about paths
-libfind_pkg_check_modules(OPENNI_PKGCONF OPENNI)
+libfind_pkg_check_modules(OpenNI_PKGCONF OpenNI)
 
 # Include dir
-find_path(OPENNI_INCLUDE_DIR
+find_path(OpenNI_INCLUDE_DIR
   NAMES XnCppWrapper.h
-  PATHS ${OPENNI_PKGCONF_INCLUDE_DIRS} /usr/include/ni
+  PATHS ${OpenNI_PKGCONF_INCLUDE_DIRS} /usr/include/ni
 )
 
 # also add the ni/ directory to the include path
-SET(OPENNI_INCLUDE_DIR_2 "${OPENNI_INCLUDE_DIR}/ni")
+set(OpenNI_INCLUDE_DIR_2 "${OpenNI_INCLUDE_DIR}/ni")
 
 # Finally the library itself
-find_library(OPENNI_LIBRARY
+find_library(OpenNI_LIBRARY
   NAMES OpenNI
-  PATHS ${OPENNI_PKGCONF_LIBRARY_DIRS}
+  PATHS ${OpenNI_PKGCONF_LIBRARY_DIRS}
 )
 
 # Set the include dir variables and the libraries and let libfind_process do the rest.
 # NOTE: Singular variables for this library, plural for libraries this this lib depends on.
-set(OPENNI_PROCESS_INCLUDES OPENNI_INCLUDE_DIR OPENNI_INCLUDE_DIR_2)
-set(OPENNI_PROCESS_LIBS OPENNI_LIBRARY)
-libfind_process(OPENNI)
+set(OpenNI_PROCESS_INCLUDES OpenNI_INCLUDE_DIR OpenNI_INCLUDE_DIR_2)
+set(OpenNI_PROCESS_LIBS OpenNI_LIBRARY)
+libfind_process(OpenNI)
