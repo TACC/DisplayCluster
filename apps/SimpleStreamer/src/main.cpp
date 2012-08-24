5e1d18982e36af7b0cdda5cf51ac8028c93f33ca
Greg Johnson <johnson.gp@gmail.com>
2012-08-24 14:29:38 -0500

    added libDisplayCluster library for remote streaming, and SimpleStreamer application that demonstrates its use.

---
+++ apps/SimpleStreamer/src/main.cpp
@@ -0,0 +1,173 @@
+#include <string>
+#include <iostream>
+
+#ifdef __APPLE__
+    #include <OpenGL/gl.h>
+    #include <GLUT/glut.h>
+#else
+    #include <GL/gl.h>
+    #include <GL/glut.h>
+#endif
+
+// DisplayCluster streaming
+#include <dcStream.h>
+
+std::string streamName = "SimpleStreamer";
+bool parallelStreaming = false;
+int segmentSize = 512;
+char * hostname = NULL;
+DcSocket * socket = NULL;
+
+void syntax(char * app);
+void display();
+
+int main(int argc, char **argv)
+{
+    // read command-line arguments
+    for(int i=1; i<argc; i++)
+    {
+        if(argv[i][0] == '-')
+        {
+            switch(argv[i][1])
+            {
+                case 'n':
+                    if(i+1 < argc)
+                    {
+                        streamName = argv[i+1];
+                        i++;
+                    }
+                    break;
+                case 'p':
+                    parallelStreaming = true;
+                    break;
+                case 's':
+                    if(i+1 < argc)
+                    {
+                        segmentSize = atoi(argv[i+1]);
+                        i++;
+                    }
+                    break;
+                default:
+                    syntax(argv[0]);
+            }
+        }
+        else if(i == argc-1)
+        {
+            hostname = argv[i];
+        }
+    }
+
+    if(hostname == NULL)
+    {
+        syntax(argv[0]);
+    }
+
+    // setup GLUT / OpenGL
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(1024, 768);
+
+    glutCreateWindow("SimpleStreamer");
+
+    // the display function will be called continuously
+    glutDisplayFunc(display);
+    glutIdleFunc(display);
+
+    glClearColor(0.5, 0.5, 0.5, 1.);
+
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+
+    // connect to DisplayCluster
+    socket = dcStreamConnect(hostname);
+
+    if(socket == NULL)
+    {
+        std::cerr << "could not connect to DisplayCluster host: " << hostname << std::endl;
+        return 1;
+    }
+
+    // enter the main loop
+    glutMainLoop();
+
+    return 0;
+}
+
+
+void syntax(char * app)
+{
+    std::cerr << "syntax: " << app << " [options] <hostname>" << std::endl;
+    std::cerr << "options:" << std::endl;
+    std::cerr << " -p                   enable parallel streaming (default disabled)" << std::endl;
+    std::cerr << " -s <segment size>    set parallel streaming segment size (default 512)" << std::endl;
+
+    exit(1);
+}
+
+void display()
+{
+    // angle of camera rotation
+    static float angle = 0;
+
+    // clear color / depth buffers and setup view
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    float size = 2.;
+    glOrtho(-size, size, -size, size, -size, size);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glRotatef(angle, 0.0, 1.0, 1.0);
+
+    // render the teapot
+    glutSolidTeapot(1.);
+
+    // send to DisplayCluster
+
+    // get current window dimensions
+    int windowWidth = glutGet(GLUT_WINDOW_WIDTH);
+    int windowHeight = glutGet(GLUT_WINDOW_HEIGHT);
+
+    // grab the image data from OpenGL
+    unsigned char * imageData = (unsigned char *)malloc(windowWidth * windowHeight * 4);
+    glReadPixels(0,0,windowWidth,windowHeight, GL_RGBA, GL_UNSIGNED_BYTE, (void *)imageData);
+
+    bool success;
+
+    if(parallelStreaming == true)
+    {
+        // use a streaming segment size of roughly <segmentSize> x <segmentSize> pixels
+        std::vector<DcStreamParameters> parameters = dcStreamGenerateParameters(streamName, 0, segmentSize,segmentSize, 0,0,windowWidth,windowHeight, windowWidth,windowHeight);
+
+        // finally, send it to DisplayCluster
+        success = dcStreamSend(socket, imageData, 0,0,windowWidth,0,windowHeight, RGBA, parameters);
+    }
+    else
+    {
+        // use a single streaming segment for the full window
+        DcStreamParameters parameters = dcStreamGenerateParameters(streamName, 0, 0,0,windowWidth,windowHeight, windowWidth,windowHeight);
+
+        // finally, send it to DisplayCluster
+        success = dcStreamSend(socket, imageData, 0,0,windowWidth,0,windowHeight, RGBA, parameters);
+    }
+
+    if(success == false)
+    {
+        std::cerr << "failure in dcStreamSend()" << std::endl;
+        exit(1);
+    }
+
+    // and free the allocated image data
+    free(imageData);
+
+    glutSwapBuffers();
+
+    // increment rotation angle
+    angle += 1.;
+}
---
+++ src/lib/dcStream.cpp
@@ -0,0 +1,396 @@
+/*********************************************************************/
+/* Copyright (c) 2011 - 2012, The University of Texas at Austin.     */
+/* All rights reserved.                                              */
+/*                                                                   */
+/* Redistribution and use in source and binary forms, with or        */
+/* without modification, are permitted provided that the following   */
+/* conditions are met:                                               */
+/*                                                                   */
+/*   1. Redistributions of source code must retain the above         */
+/*      copyright notice, this list of conditions and the following  */
+/*      disclaimer.                                                  */
+/*                                                                   */
+/*   2. Redistributions in binary form must reproduce the above      */
+/*      copyright notice, this list of conditions and the following  */
+/*      disclaimer in the documentation and/or other materials       */
+/*      provided with the distribution.                              */
+/*                                                                   */
+/*    THIS  SOFTWARE IS PROVIDED  BY THE  UNIVERSITY OF  TEXAS AT    */
+/*    AUSTIN  ``AS IS''  AND ANY  EXPRESS OR  IMPLIED WARRANTIES,    */
+/*    INCLUDING, BUT  NOT LIMITED  TO, THE IMPLIED  WARRANTIES OF    */
+/*    MERCHANTABILITY  AND FITNESS FOR  A PARTICULAR  PURPOSE ARE    */
+/*    DISCLAIMED.  IN  NO EVENT SHALL THE UNIVERSITY  OF TEXAS AT    */
+/*    AUSTIN OR CONTRIBUTORS BE  LIABLE FOR ANY DIRECT, INDIRECT,    */
+/*    INCIDENTAL,  SPECIAL, EXEMPLARY,  OR  CONSEQUENTIAL DAMAGES    */
+/*    (INCLUDING, BUT  NOT LIMITED TO,  PROCUREMENT OF SUBSTITUTE    */
+/*    GOODS  OR  SERVICES; LOSS  OF  USE,  DATA,  OR PROFITS;  OR    */
+/*    BUSINESS INTERRUPTION) HOWEVER CAUSED  AND ON ANY THEORY OF    */
+/*    LIABILITY, WHETHER  IN CONTRACT, STRICT  LIABILITY, OR TORT    */
+/*    (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING IN ANY WAY OUT    */
+/*    OF  THE  USE OF  THIS  SOFTWARE,  EVEN  IF ADVISED  OF  THE    */
+/*    POSSIBILITY OF SUCH DAMAGE.                                    */
+/*                                                                   */
+/* The views and conclusions contained in the software and           */
+/* documentation are those of the authors and should not be          */
+/* interpreted as representing official policies, either expressed   */
+/* or implied, of The University of Texas at Austin.                 */
+/*********************************************************************/
+
+#include "dcStream.h"
+#include "../NetworkProtocol.h"
+#include "../MessageHeader.h"
+#include "../ParallelPixelStreamSegmentParameters.h"
+#include "../log.h"
+#include <QtNetwork/QTcpSocket>
+#include <QtCore>
+#include <cmath>
+#include <turbojpeg.h>
+
+struct DcImage {
+    unsigned char * imageBuffer;
+    int width;
+    int pitch;
+    int height;
+    PIXEL_FORMAT pixelFormat;
+    char * jpegData;
+    int jpegSize;
+};
+
+// enum PIXEL_FORMAT { RGB, RGBA, ARGB, BGR, BGRA, ABGR };
+int dcBytesPerPixel[] = { 3, 4, 4, 3, 4, 4 };
+
+DcImage dcStreamComputeJpegMapped(const DcImage & dcImage);
+
+
+DcSocket * dcStreamConnect(const char * hostname)
+{
+    DcSocket * socket = new QTcpSocket();
+
+    // open connection
+    socket->connectToHost(hostname, 1701);
+
+    if(socket->waitForConnected() != true)
+    {
+        put_flog(LOG_ERROR, "could not connect to host %s", hostname);
+
+        return NULL;
+    }
+
+    // handshake
+    while(socket->waitForReadyRead() && socket->bytesAvailable() < (int)sizeof(int32_t))
+    {
+    #ifndef _WIN32
+        usleep(10);
+    #endif
+    }
+
+    int32_t protocolVersion = -1;
+    socket->read((char *)&protocolVersion, sizeof(int32_t));
+
+    if(protocolVersion != NETWORK_PROTOCOL_VERSION)
+    {
+        socket->disconnectFromHost();
+
+        put_flog(LOG_ERROR, "unsupported protocol version %i != %i", protocolVersion, NETWORK_PROTOCOL_VERSION);
+
+        return NULL;
+    }
+
+    return socket;
+}
+
+void dcStreamDisconnect(DcSocket * socket)
+{
+    delete socket;
+
+    socket = NULL;
+}
+
+DcStreamParameters dcStreamGenerateParameters(std::string name, int sourceIndex, int x, int y, int width, int height, int totalWidth, int totalHeight)
+{
+    DcStreamParameters parameters;
+
+    parameters.name = name;
+    parameters.sourceIndex = sourceIndex;
+    parameters.x = x;
+    parameters.y = y;
+    parameters.width = width;
+    parameters.height = height;
+    parameters.totalWidth = totalWidth;
+    parameters.totalHeight = totalHeight;
+
+    return parameters;
+}
+
+std::vector<DcStreamParameters> dcStreamGenerateParameters(std::string name, int firstSourceIndex, int nominalSegmentWidth, int nominalSegmentHeight, int x, int y, int width, int height, int totalWidth, int totalHeight)
+{
+    // segment dimensions will be approximately nominalSegmentWidth x nominalSegmentHeight
+
+    // number of subdivisions in each dimension
+    int numSubdivisionsX = (int)floor((float)width / (float)nominalSegmentWidth + 0.5);
+    int numSubdivisionsY = (int)floor((float)height / (float)nominalSegmentHeight + 0.5);
+
+    // now, create parameters for each segment
+    std::vector<DcStreamParameters> parameters;
+
+    for(int i=0; i<numSubdivisionsX; i++)
+    {
+        for(int j=0; j<numSubdivisionsY; j++)
+        {
+            DcStreamParameters p;
+
+            p.name = name;
+            p.sourceIndex = firstSourceIndex;
+            p.x = x + i * (int)((float)width / (float)numSubdivisionsX);
+            p.y = y + j * (int)((float)height / (float)numSubdivisionsY);
+            p.width = (int)((float)width / (float)numSubdivisionsX);
+            p.height = (int)((float)height / (float)numSubdivisionsY);
+            p.totalWidth = totalWidth;
+            p.totalHeight = totalHeight;
+
+            parameters.push_back(p);
+
+            firstSourceIndex++;
+        }
+    }
+
+    return parameters;
+}
+
+bool dcStreamSend(DcSocket * socket, unsigned char * imageBuffer, int imageX, int imageY, int imageWidth, int imagePitch, int imageHeight, PIXEL_FORMAT pixelFormat, DcStreamParameters parameters)
+{
+    // compute imagePitch if necessary, assuming imageBuffer isn't padded
+    if(imagePitch == 0)
+    {
+        imagePitch = imageWidth * dcBytesPerPixel[pixelFormat];
+    }
+
+    // compute JPEG from imageBuffer corresponding to parameters
+    unsigned char * segmentImageBuffer = imageBuffer + (parameters.y - imageY)*imagePitch + (parameters.x - imageX)*dcBytesPerPixel[pixelFormat];
+
+    char * jpegData = NULL;
+    int jpegSize = 0;
+
+    bool success = dcStreamComputeJpeg(segmentImageBuffer, parameters.width, imagePitch, parameters.height, pixelFormat, &jpegData, jpegSize);
+
+    if(success == false)
+    {
+        free(jpegData);
+        return false;
+    }
+
+    success = dcStreamSendJpeg(socket, parameters, jpegData, jpegSize);
+
+    free(jpegData);
+    return success;
+}
+
+bool dcStreamSend(DcSocket * socket, unsigned char * imageBuffer, int imageX, int imageY, int imageWidth, int imagePitch, int imageHeight, PIXEL_FORMAT pixelFormat, std::vector<DcStreamParameters> parameters)
+{
+    // compute imagePitch if necessary, assuming imageBuffer isn't padded
+    if(imagePitch == 0)
+    {
+        imagePitch = imageWidth * dcBytesPerPixel[pixelFormat];
+    }
+
+    // compute JPEGs from imageBuffer corresponding to parameters vector
+    std::vector<DcImage> dcImages;
+
+    for(unsigned int i=0; i<parameters.size(); i++)
+    {
+        DcImage d;
+
+        d.imageBuffer = imageBuffer + (parameters[i].y - imageY)*imagePitch + (parameters[i].x - imageX)*dcBytesPerPixel[pixelFormat];
+        d.width = parameters[i].width;
+        d.pitch = imagePitch;
+        d.height = parameters[i].height;
+        d.pixelFormat = pixelFormat;
+        d.jpegData = NULL;
+        d.jpegSize = 0;
+
+        dcImages.push_back(d);
+    }
+
+    // create JPEGs for each segment, in parallel
+    
+    dcImages = QtConcurrent::blockingMapped<std::vector<DcImage> >(dcImages, &dcStreamComputeJpegMapped);
+
+    // send each segment, and return true if we were successful for all segments
+    bool allSuccess = true;
+
+    for(unsigned int i=0; i<dcImages.size(); i++)
+    {
+        // jpegSize == 0 indicates an error
+        if(dcImages[i].jpegSize == 0)
+        {
+            allSuccess = false;
+        }
+        else
+        {
+            bool sendSuccess = dcStreamSendJpeg(socket, parameters[i], dcImages[i].jpegData, dcImages[i].jpegSize);
+
+            if(sendSuccess == false)
+            {
+                allSuccess = false;
+            }
+        }
+
+        free(dcImages[i].jpegData);
+    }
+
+    return allSuccess;
+}
+
+bool dcStreamSendJpeg(DcSocket * socket, DcStreamParameters parameters, const char * jpegData, int jpegSize)
+{
+    if(socket == NULL)
+    {
+        put_flog(LOG_ERROR, "socket is NULL");
+
+        return false;
+    }
+
+    if(socket->state() != QAbstractSocket::ConnectedState)
+    {
+        put_flog(LOG_ERROR, "socket is not connected");
+
+        return false;
+    }
+
+    // send the parameters and image data
+    MessageHeader mh;
+    mh.size = sizeof(ParallelPixelStreamSegmentParameters) + jpegSize;
+    mh.type = MESSAGE_TYPE_PARALLEL_PIXELSTREAM;
+
+    // add the truncated URI to the header
+    size_t len = parameters.name.copy(mh.uri, MESSAGE_HEADER_URI_LENGTH - 1);
+    mh.uri[len] = '\0';
+
+    // send the header
+    int sent = socket->write((const char *)&mh, sizeof(MessageHeader));
+
+    while(sent < (int)sizeof(MessageHeader))
+    {
+        sent += socket->write((const char *)&mh + sent, sizeof(MessageHeader) - sent);
+    }
+
+    // send the message
+
+    // part 1: parameters
+    ParallelPixelStreamSegmentParameters p;
+
+    p.sourceIndex = parameters.sourceIndex;
+    p.x = parameters.x;
+    p.y = parameters.y;
+    p.width = parameters.width;
+    p.height = parameters.height;
+    p.totalWidth = parameters.totalWidth;
+    p.totalHeight = parameters.totalHeight;
+
+    sent = socket->write((const char *)&(p), sizeof(ParallelPixelStreamSegmentParameters));
+
+    while(sent < (int)sizeof(ParallelPixelStreamSegmentParameters))
+    {
+        sent += socket->write((const char *)&(p) + sent, sizeof(ParallelPixelStreamSegmentParameters) - sent);
+    }
+
+    // part 2: image data
+    sent = socket->write(jpegData, jpegSize);
+
+    while(sent < jpegSize)
+    {
+        sent += socket->write(jpegData + sent, jpegSize - sent);
+    }
+
+    // wait for acknowledgment
+    while(socket->waitForReadyRead() && socket->bytesAvailable() < 3)
+    {
+#ifndef _WIN32
+        usleep(10);
+#endif
+    }
+
+    socket->read(3);
+
+    return true;
+}
+
+bool dcStreamComputeJpeg(unsigned char * imageBuffer, int width, int pitch, int height, PIXEL_FORMAT pixelFormat, char ** jpegData, int & jpegSize)
+{
+    // use libjpeg-turbo for JPEG conversion
+
+    // use a new handle each time for thread-safety
+    tjhandle tjHandle = tjInitCompress();
+
+    // compute pitch if necessary, assuming imageBuffer isn't padded
+    if(pitch == 0)
+    {
+        pitch = width * dcBytesPerPixel[pixelFormat];
+    }
+
+    // map pixel format to the libjpeg-turbo equivalent
+    int tjPixelFormat;
+
+    // enum PIXEL_FORMAT { RGB, RGBA, ARGB, BGR, BGRA, ABGR };
+    switch(pixelFormat)
+    {
+        case RGB:
+            tjPixelFormat = TJPF_RGB;
+            break;
+        case RGBA:
+            tjPixelFormat = TJPF_RGBX;
+            break;
+        case ARGB:
+            tjPixelFormat = TJPF_XRGB;
+            break;
+        case BGR:
+            tjPixelFormat = TJPF_BGR;
+            break;
+        case BGRA:
+            tjPixelFormat = TJPF_BGRX;
+            break;
+        case ABGR:
+            tjPixelFormat = TJPF_XBGR;
+            break;
+        default:
+            put_flog(LOG_ERROR, "unknown pixel format");
+            return false;
+    }
+
+    unsigned char ** tjJpegBuf;
+    unsigned char * tjJpegBufPtr = NULL;
+    tjJpegBuf = &tjJpegBufPtr;
+    unsigned long tjJpegSize = 0;
+    int tjJpegSubsamp = TJSAMP_444;
+    int tjJpegQual = 75;
+    int tjFlags = 0;
+
+    int success = tjCompress2(tjHandle, imageBuffer, width, pitch, height, tjPixelFormat, tjJpegBuf, &tjJpegSize, tjJpegSubsamp, tjJpegQual, tjFlags);
+
+    if(success != 0)
+    {
+        put_flog(LOG_ERROR, "libjpeg-turbo image conversion failure");
+
+        jpegSize = 0;
+
+        return false;
+    }
+
+    // move the JPEG buffer to our own memory and free the libjpeg-turbo allocated memory
+    *jpegData = (char *)realloc((void *)*jpegData, tjJpegSize);
+    memcpy(*jpegData, tjJpegBufPtr, tjJpegSize);
+    tjFree(tjJpegBufPtr);
+
+    jpegSize = tjJpegSize;
+
+    return true;
+}
+
+DcImage dcStreamComputeJpegMapped(const DcImage & dcImage)
+{
+    DcImage newDcImage = dcImage;
+
+    dcStreamComputeJpeg(newDcImage.imageBuffer, newDcImage.width, newDcImage.pitch, newDcImage.height, newDcImage.pixelFormat, &newDcImage.jpegData, newDcImage.jpegSize);
+
+    return newDcImage;
+}
