50713f7bfa3c0b9411fc4f53723bf80aa1aac71a
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2013-06-18 16:38:19 +0200

    Limit to width as well (#27)

--- src/ContentWindowManager.cpp
+++ src/ContentWindowManager.cpp
@@ -58,8 +58,14 @@ ContentWindowManager::ContentWindowManager(boost::shared_ptr<Content> content)
     h_ = contentHeight_ == 0 ? 1. : double(contentHeight_) /
                                     double(g_configuration->getTotalHeight());
     h_ = std::min( h_, 1. );
-    y_ = (1. - h_) * .5;
     w_ = configAR * contentAR * h_;
+    if( w_ > 1. )
+    {
+        h_ /= w_;
+        w_ /= w_;
+    }
+
+    y_ = (1. - h_) * .5;
     x_ = (1. - w_) * .5;
 
     // default to centered
