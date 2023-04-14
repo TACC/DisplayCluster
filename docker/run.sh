2ff50272698944da6cbef1a169860b1e3f8f1022
Greg Abram <gda@tacc.utexas.edu>
2023-04-14 15:59:26 -0500

    python2

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,1 @@
-docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -h $HOSTNAME -v $HOME/.Xauthority:/home/lyonn/.Xauthority gregabram/displaycluster
-
-
-
-# docker run -it -e"DISPLAY=host.docker.internal:0" -v /tmp/.X11-unix:/tmp/.X11-unix:rw  gregabram/displaycluster
+docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -h $HOSTNAME -v $HOME/.Xauthority:/home/gda/.Xauthority gregabram/displaycluster
