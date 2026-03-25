cc5666ae46add7b49387c75fce06d26e9879a571
Greg Abram <gda@tacc.utexas.edu>
2026-03-25 15:04:09 -0500

    Revert "more on hide/reveal"
    This reverts commit 627923440bdb79af466ac837014f011a49b5efca.

--- src/DisplayGroupListWidgetProxy.h
+++ src/DisplayGroupListWidgetProxy.h
@@ -60,7 +60,6 @@ class DisplayGroupListWidgetProxy : public DisplayGroupInterface {
     private slots:
 
         void moveListWidgetItemToFront(QListWidgetItem * item);
-        void onHideCheckboxChanged(int state);
 
     private:
 
